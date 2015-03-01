e99b9751434fcd151308de98c2b8052a6290f13c
Robert Northard <robertnorthard@googlemail.com>
2015-03-01 13:06:49 +0000

    Refactored DB Connection class to use loggers and added explicit private constructor.

--- src/main/java/org/jenkinsci/plugins/environmentdashboard/utils/DBConnection.java
+++ src/main/java/org/jenkinsci/plugins/environmentdashboard/utils/DBConnection.java
@@ -1,60 +1,75 @@
 package org.jenkinsci.plugins.environmentdashboard.utils;
 
-import hudson.model.Hudson;
-
+import java.io.File;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.logging.Logger;
 
-import java.io.File;
+import jenkins.model.Jenkins;
 
 /**
- * Singleton class to represent a single DB connection. 
+ * Singleton class to represent a single DB connection.
+ * 
  * @author robertnorthard
- * @date 18/10/2014
+ * @date 18/10/2014, 01/03/2015
  */
 public class DBConnection {
 
-	private static Connection con = null;
-	
-	/**
-	 * Return a database connection object.
-	 * @return a database connection object
-	 */
-	public static Connection getConnection(){
-		
-		// Generate connection String for DB driver.
-		String dbConnectionString = "jdbc:h2:" + Hudson.getInstance().root.toString() +
-									 File.separator + "jenkins_dashboard" + ";MVCC=true";
-		
-		//Load driver and connect to DB
-		try { 
-			Class.forName("org.h2.Driver");
-			DBConnection.con = DriverManager.getConnection(dbConnectionString);
-		} catch (ClassNotFoundException e) {
-			System.err.println("WARN: Could not acquire Class org.h2.Driver.");
-		} catch (SQLException e){
-			System.err.println("WARN: Could not acquire connection to H2 DB.");
-		}
-		return con;
-	}
-	
-	/**
-	 * Close Database Connection
-	 * @return true if database connection closed successful,
-	 * 			 else false if connection not closed or SQLException.
-	 */
-	public static boolean closeConnection(){
-		
-		//Prevent unchecked NullPointerException
-		if(DBConnection.con != null){
-			try {
-				DBConnection.con.close();
-				return true;
-			} catch (SQLException error) { System.err.println("E5"); return false; }
-		}
-		//default  - failed to close
-		return false;
-	}
+    private static final Logger LOGGER = Logger.getLogger(DBConnection.class.getName());
+
+    private static Connection con = null;
+
+    /**
+     * Added explicit private constructor as this is a utility class.
+     */
+    private DBConnection() {
+
+    }
+
+    /**
+     * Return a database connection object.
+     * 
+     * @return a database connection object
+     */
+    public static Connection getConnection() {
+
+        // Generate connection String for DB driver.
+        String dbConnectionString = "jdbc:h2:"
+                + Jenkins.getInstance().root.toString() + File.separator
+                + "jenkins_dashboard";
+
+        // Load driver and connect to DB
+        try {
+            Class.forName("org.h2.Driver");
+            DBConnection.con = DriverManager.getConnection(dbConnectionString);
+        } catch (ClassNotFoundException e) {
+            LOGGER.info("WARN: Could not acquire Class org.h2.Driver." + e);
+        } catch (SQLException e) {
+            LOGGER.info("WARN: Could not acquire connection to H2 DB." + e);
+        }
+        return con;
+    }
+
+    /**
+     * Close Database Connection
+     * 
+     * @return true if database connection closed successful, else false if
+     *         connection not closed or SQLException.
+     */
+    public static boolean closeConnection() {
+
+        // Prevent unchecked NullPointerException
+        if (DBConnection.con != null) {
+            try {
+                DBConnection.con.close();
+                return true;
+            } catch (SQLException e) {
+                LOGGER.info("E5: " + e);
+                return false;
+            }
+        }
+        // default - failed to close
+        return false;
+    }
 }
-	
